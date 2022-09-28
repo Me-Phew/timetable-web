46b7bc9b14723c04fb56b8f311fb3e8e141f2b77
Me-Phew <basmateusz@wp.pl>
2022-09-28 18:28:00 +0200

    Implement basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import Timetable from "./components/Timetable.vue";
+import { onMounted, reactive } from "vue";
+import TimetableItem from "./components/TimetableItem.vue";
 import axios from "axios";
+import { useDebounceFn } from "@vueuse/core";
 
-const stops = ref(null);
+const state = reactive({
+  stops: [],
+  foundStops: [],
+});
 
-onMounted(async () => {
-  stops.value = await axios.get(
-    ""
-  );
-})
+const findStops = async (e) => {
+  const q = e.target.value;
+  const response = await axios.get("http://127.0.0.1:8000/timetable-api/stops");
+  state.foundStops = response.data.st.filter((stop) => {
+    return stop.nm.toLowerCase().includes(q.toLowerCase());
+  }, 200);
+};
+
+const findStopsDebounce = useDebounceFn((e) => findStops(e));
+
+const checkShop = (stop, nr) => {
+  return stop.nr === nr;
+};
+
+const addStop = (nr) => {
+  if (!state.stops.find((stop) => checkShop(stop, nr))) {
+    state.stops.push(state.foundStops.find((stop) => checkShop(stop, nr)));
+  }
+};
 </script>
 
 <template>
   <header>
-    {{ stops }}
+    <input
+      type="search"
+      name=""
+      id=""
+      @input="findStopsDebounce"
+      @keyup.enter="findStops"
+      class="search-stops"
+    />
+    <ol v-if="state.foundStops" class="found-stops">
+      <li
+        v-for="stop in state.foundStops"
+        :key="stop.nr"
+        @click="addStop(stop.nr)"
+      >
+        {{ stop.nm }} ({{ stop.nr }})
+      </li>
+    </ol>
   </header>
 
   <main>
-    <Timetable />
+    <TimetableItem v-for="stop in state.stops" :key="stop.nr" :stop="stop" />
   </main>
 </template>
 
 <style scoped>
+.search-stops {
+  outline: none;
+  border: 0;
+  border-bottom: 2px solid var(--color-border);
+}
+.search-stops:not(:focus) + .found-stops:not(:hover) {
+  display: none;
+}
+
 header {
   line-height: 1.5;
 }
---
+++ src/components/TimetableItem.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  stop: Object,
+});
+</script>
+
+<template>
+  <div class="wrapper">
+    <p>{{ stop.nm }} ({{ stop.nr }})</p>
+    <table>
+      <thead>
+        <th>Linia</th>
+        <th>Kierunek</th>
+        <th>Odjazd</th>
+      </thead>
+      <tbody>
+        <tr v-for="line in stop.sd" :key="line.li">
+          <td>{{ line.li }}</td>
+          <td>{{ line.di }}</td>
+          <td>{{ line.de }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  background-color: var(--color-background-soft);
+  padding: 1rem;
+  border-radius: 0.375rem;
+  margin: 0.5rem;
+
+  th {
+    color: hsla(160, 100%, 37%, 1);
+  }
+}
+</style>
